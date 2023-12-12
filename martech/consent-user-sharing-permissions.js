ee8941da13de74f6b3d3f01206d88c544ca43f22
Todd Croak-Falen <toddcf@gmail.com>
2023-12-11 21:05:34 -0800

    Add User Sharing Permissions

---
+++ martech/consent-user-sharing-permissions.js
@@ -0,0 +1,11 @@
+// Make sure no 'null' values or empty strings get passed, or AEP will have an ingestion failure:
+if (
+  !!window.digitalData &&
+  !!window.digitalData.user &&
+  !!window.digitalData.user.profile &&
+  !!window.digitalData.user.profile.contactPreferences &&
+  !!window.digitalData.user.profile.contactPreferences.doNotShare &&
+  typeof window.digitalData.user.profile.contactPreferences.doNotShare === 'string'
+) {
+  return window.digitalData.user.profile.contactPreferences.doNotShare;
+}
