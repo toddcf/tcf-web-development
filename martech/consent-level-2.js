91d9b08d745ce2a7129db1bd955393b23cbd7bdb
Todd Croak-Falen <toddcf@gmail.com>
2023-12-11 21:40:15 -0800

    Convert Consent Level 2 to optional chaining

--- martech/consent-level-2.js
+++ martech/consent-level-2.js
@@ -1,5 +1,11 @@
 let consentLevel2 = false;
-if (!!window.digitalData && !!window.digitalData.user && !!window.digitalData.user.consent && window.digitalData.user.consent.C0002 === true) {
-    consentLevel2 = true;
+if (window.digitalData?.user?.consent?.C0002 === true) {
+  consentLevel2 = true;
 }
 return consentLevel2;
+
+// let consentLevel2 = false;
+// if (!!window.digitalData && !!window.digitalData.user && !!window.digitalData.user.consent && window.digitalData.user.consent.C0002 === true) {
+//     consentLevel2 = true;
+// }
+// return consentLevel2;
