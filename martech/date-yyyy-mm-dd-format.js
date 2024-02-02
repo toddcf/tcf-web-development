ce268c577872b7b94952e4e1af263db7fb7974df
Todd Croak-Falen <toddcf@gmail.com>
2024-02-01 18:59:12 -0800

    Remove the vast majority of comments so side-scrolling is unnecessary

--- martech/date-yyyy-mm-dd-format.js
+++ martech/date-yyyy-mm-dd-format.js
@@ -1,18 +1,11 @@
-// Pass a date object into this data element and it will reformat it to 'YYYY-MM-DD' (with hyphens).
-// Arrays of date objects will be convered to a 'YYYY-MM-DD,YYYY-MM-DD,YYYY-MM-DD' string.
-// 'any' strings will pass through unmodified.
-// Unwanted input formats are wiped to empty strings so that they will be flagged by ObservePoint.
-const inputDate = event; // The argument that is passed into this data element.
-let outputYYYYMMDD = (!!inputDate) ? inputDate : ''; // Defaults for output (in case the value is 'any', or nonexistent).
+const inputDate = event;
+let outputYYYYMMDD = (!!inputDate) ? inputDate : '';
 const formattedDateArr = [];
 
-// Converts a single date object to YYYY-MM-DD:
 const formatYYYYMMDD = (singleDateObj) => {
-  // Defaults assist with debugging:
   let yyyy = 'YYYY';
   let mm = 'MM';
   let dd = 'DD';
-  // Reformats date object. 'any' strings skip this part.
   if (typeof singleDateObj.getMonth === 'function') {
     yyyy = singleDateObj.getFullYear();
     mm = singleDateObj.getMonth() + 1;
@@ -23,29 +16,23 @@ const formatYYYYMMDD = (singleDateObj) => {
   return yyyy + '-' + mm + '-' + dd;
 }
 
-// Determine how and when to fire the formatYYYYMMDD function:
 if (!!inputDate) {
   if (Array.isArray(inputDate)) {
     // If inputDate is an array:
     if (inputDate.length > 0 && typeof inputDate[0].getMonth === 'function') {
-      // Converts an array of date objects to a string of YYYY-MM-DD,YYYY-MM-DD,YYYY-MM-DD:
       inputDate.forEach((dateObj) => {
-        // If it is a date object:
         if (typeof dateObj.getMonth === 'function') {
-          formattedDateArr.push(formatYYYYMMDD(dateObj)); // Format each date and push it to the new array.
+          formattedDateArr.push(formatYYYYMMDD(dateObj));
         }
       });
-      // Convert the array to a string:
-      outputYYYYMMDD = (formattedDateArr.length > 0) ? formattedDateArr.join() : ''; // Comma-delimited. DO NOT encode commas. Convert an empty array to an empty string so ObservePoint will flag it.
+      outputYYYYMMDD = (formattedDateArr.length > 0) ? formattedDateArr.join() : '';
     } else {
-      // If inputDate was an array of something other than date objects, wipe it so ObservePoint will flag it:
       outputYYYYMMDD = '';
     }
   } else if (typeof inputDate.getMonth === 'function') {
     // If inputDate is an individual date object:
     outputYYYYMMDD = formatYYYYMMDD(inputDate);
   } else if (typeof inputDate === 'string' && inputDate !== 'any') {
-    // 'any' strings pass through.  All other strings get wiped (such as date objects that were erroneously converted to strings by the app) so that ObservePoint flags them as missing and we know to debug them.
     outputYYYYMMDD = '';
   }
 }
